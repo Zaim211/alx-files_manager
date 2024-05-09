cabf9333fab01bf86a0295256b1ccd495b30e445
Zaim211 <yousseftitazaim@gmail.com>
2024-05-09 21:36:24 +0000

    Done

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,14 @@
 import { MongoClient } from 'mongodb';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'file_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'file_manager';
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
     this.client.connect().then(() => {
-      this.db = this.connect.db(`${DB_DATABASE}`);
+      this.db = this.client.db(`${DATABASE}`);
     }).catch((error) => {
       console.log(error);
     });
@@ -20,14 +20,14 @@ class DBClient {
 
   async nbUsers() {
     const users = this.db.collection('users')
-    const numberOfUsers = await users.countDocument();
+    const numberOfUsers = await users.countDocuments();
     return numberOfUsers;
   }
 
   async nbFiles() {
     const files = this.db.collection('files');
-    const numberOfFiles = await files.countDocument();
-    return numerOfFiles;
+    const numberOfFiles = await files.countDocuments();
+    return numberOfFiles;
   }
 }
 
